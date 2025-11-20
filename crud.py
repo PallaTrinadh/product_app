88f715eae70cacbcac823d25f3acad495b73ef83
Trinadh Palla <tpalla@msa.miraclesoft.com>
2025-11-20 20:29:28 +0530

    Merge demo

--- crud.py
+++ crud.py
@@ -17,21 +17,21 @@ def create_product_order(db: Session, order_data: schemas.OrderCreate):
 
     total = 0
 
-    # Check each product stock
-    for item in order_data.items:
-        if item.product_id not in products:
-            raise HTTPException(404, f"Product {item.product_id} not found")
+    # # Check each product stock
+    # for item in order_data.items:
+    #     if item.product_id not in products:
+    #         raise HTTPException(404, f"Product {item.product_id} not found")
 
-        product = products[item.product_id]
-        if product.stock < item.quantity:
-            # FAIL ORDER
-            order = models.Order(id=order_id, status=models.OrderStatus.FAILED, total=0)
-            # db.add(order)
-            # db.commit()
-            db.refresh(order)
-            return order
+    #     product = products[item.product_id]
+    #     if product.stock < item.quantity:
+    #         # FAIL ORDER
+    #         order = models.Order(id=order_id, status=models.OrderStatus.FAILED, total=0)
+    #         # db.add(order)
+    #         # db.commit()
+    #         db.refresh(order)
+    #         return order
 
-        total += product.price * item.quantity
+    #     total += product.price * item.quantity
 
     order = models.Order(id=order_id, status=models.OrderStatus.SUCCESS, total=total)
     db.add(order)
